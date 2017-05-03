ea7efc9e63de99dfe88fbb18b1de9a0fe2734da3
Cassidy <tarng.cassidy@gmail.com>
2017-05-02 17:08:03 -0700

    10 queries total

--- Main.py
+++ Main.py
@@ -5,13 +5,16 @@ collection = db['flight']
 
 def mainPrompt():
     print('Welcome! What would you like to do today? Here are your choices:'
-          '\n1 - Find the number of flights coming from a specific state'
-          '\n2 - Lists the months and the average departure delay experienced in those months'
-          '\n3 - Find the average flight time from flights between certain airports'
-          '\n4 - Find all airport codes in a certain state'
-          '\n5 - Find the probability that a flight going to an airport will be cancelled'
-          '\n6 - Find the average departure delay of a certain airport'
-          '\n7 - Find the average arrival delay time for a certain flight carrier')
+          '\n1  - Find the number of flights coming from a specific state'
+          '\n2  - Lists the months and the average departure delay experienced in those months'
+          '\n3  - Find the average flight time from flights between certain airports'
+          '\n4  - Find all airport codes in a certain state'
+          '\n5  - Find the probability that a flight going to an airport will be cancelled'
+          '\n6  - Find the average departure delay of a certain airport'
+          '\n7  - Find the average arrival delay time for a certain flight carrier'
+          '\n8  - Lists the months and the average taxi out time in a given airport'
+          '\n9  - Find the average delay due to weather when leaving from a certain airport'
+          '\n10 - Find the number of cancelled flights in a given month and year')
     print()
     userValue = input('Enter your choice here: ')
     if userValue == '1':
@@ -35,6 +38,16 @@ def mainPrompt():
     elif userValue == '7':
         carrier = input('What carrier would you like to see? ')
         averageDelayTimePerFlightCarrier(carrier)
+    elif userValue == '8':
+        airport = input('What airport would you like to see? ')
+        averageTaxiOutTimePerMonth(airport)
+    elif userValue == '9':
+        airport = input('What airport would you like to see? ')
+        averageWeatherDelayWhenLeaving(airport)
+    elif userValue == '10':
+        month = input('Which month? ')
+        year = input('Which year? ')
+        findNumberOfCancelledFlights(month, year)
 
 # Find the number of flights coming from a specific state
 def numOfFlightsFromState(state):
@@ -75,9 +88,32 @@ def averageDepartureDelayAtAirport(airport):
     for result in results:
         print("The average departure delay from %s is %.2 minutes" % (airport, result['average_departure_delay']))
 
+# Find the average arrival delay time for a certain flight carrier
 def averageDelayTimePerFlightCarrier(carrier):
     results = collection.aggregate([{'$match': {"CARRIER": carrier}},{'$group':{'_id': "$CARRIER",'average_arrival_delay': { '$avg': "$ARR_DELAY" }}}])
     for result in results:
         print("The average delay time of %s is %2.2f minutes" % (carrier, result['average_arrival_delay']))
+
+# Lists the months and the average taxi out time in a given airport
+def averageTaxiOutTimePerMonth(airport):
+    results = collection.aggregate([{'$match': {"ORIGIN": airport}}, {'$group': {'_id': "$MONTH", 'avgTaxiOutTime': {'$avg': "$TAXI_OUT"}}}, {'$sort': {'_id': 1}}])
+    print('Month |  Taxi Out Time in Minutes')
+    print('----------------------------------')
+    for result in results:
+        if result['_id'] is not None:
+            print("%-6d|%9.2f" % (result['_id'], result['avgTaxiOutTime']))
+
+# Find the average delay due to weather when leaving from a certain airpor
+def averageWeatherDelayWhenLeaving(airport):
+    results = collection.aggregate([{'$match': {"ORIGIN": airport}}, {'$group': {'_id': "$MONTH", 'average_weather_delay': {'$avg': "$WEATHER_DELAY"}}}, {'$sort': {'_id': 1}}])
+    print('Month |  Weather Delay in Minutes')
+    print('----------------------------------')
+    for result in results:
+        if result['_id'] is not None:
+            print("%-6d|%9.2f" % (result['_id'], result['average_weather_delay']))
+
+# Lists the months and number of cancelled flights in a given year
+def findNumberOfCancelledFlights(month, year):
+    print('The number of flights cancelled in {}-{} is {} from a total of {} flights'.format(month, year, collection.find({'MONTH': int(month), 'YEAR': int(year), 'CANCELLED': 1}).count(), collection.find({'MONTH': int(month), 'YEAR': int(year)}).count()))
 
 mainPrompt()
